26c4467e1c10324ef8a470f521f0f8dab3c35db8
youj3an <youj3an@gmail.com>
2013-09-09 00:54:29 -0700

    fix end error

--- app/controllers/receive_text_controller.rb
+++ app/controllers/receive_text_controller.rb
@@ -2,6 +2,5 @@ class ReceiveTextController < ApplicationController
 	def receive_sms
 		@message = Message.find_by_keyword(params['Body'])
 		SMSLogger.log_text_message @message.body
-		end
 	end
 end
