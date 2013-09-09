45a474632f5b904206c78d08931b904c5adb052b
youj3an <youj3an@gmail.com>
2013-09-09 01:11:57 -0700

    add skip_before_filter :verify_authenticity_token to receivetextcontroller

--- app/controllers/receive_text_controller.rb
+++ app/controllers/receive_text_controller.rb
@@ -1,4 +1,6 @@
 class ReceiveTextController < ApplicationController
+	skip_before_filter :verify_authenticity_token
+
 	def receive_sms
 		@message = Message.find_by_keyword(params['Body'])
 		SMSLogger.log_text_message @message.body
