3493687c0cda3dcc8b6993787d9aa9169c3f3455
youj3an <youj3an@gmail.com>
2013-09-09 00:26:59 -0700

    add twiml to fail and success responses

--- app/controllers/receive_text_controller.rb
+++ app/controllers/receive_text_controller.rb
@@ -2,9 +2,9 @@ class ReceiveTextController < ApplicationController
 	def receive_sms
 		@message = Message.find_by_keyword(params['Body'])
 		if @message
-			@message.body
+			render 'successful message', content_type: 'text/xml'
 		else
-			render 'sample_message', :content_type => 'text/xml'
+			render 'sample_message',     content_type: 'text/xml'
 		end
 	end
 end
